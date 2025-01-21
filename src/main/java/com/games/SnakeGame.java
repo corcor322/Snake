5975ab53e919f5fbc736b66f43511dd44f3d40ff
corcor322 <corcor322@gmail.com>
2025-01-21 10:12:31 -0500

    initial commit

---
+++ src/main/java/com/games/SnakeGame.java
@@ -0,0 +1,20 @@
+package com.games;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+import static javax.swing.text.StyleConstants.setBackground;
+
+public class SnakeGame extends JPanel{
+    int boardWidth;
+    int boardHeight;
+
+    SnakeGame(int boardWidth, int boardHeight) {
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+        setPreferredSize(new Dimension(this.boardWidth, this.boardHeight));
+        setBackground(Color.pink);
+    }
+}
